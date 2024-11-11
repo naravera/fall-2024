dc7924156f2a16e50ddec36b5fda094c9177c60b
naravera <narajaazmin7@gmail.com>
2024-11-11 11:48:36 -0600

    added new gallery page

---
+++ js/gallery.js
@@ -0,0 +1,29 @@
+// A $( document ).ready() block.
+$( document ).ready(function() {
+   // console.log( "ready!" );
+
+   $("#galleryIMED1316").nanogallery2({
+    
+    thumbnailHeight:  "auto",
+    thumbnailWidth:   "450",
+    thumbnailBorderVertical: "0",
+    thumbnailBorderHorizontal: "0",
+    thumbnailLabel: {
+        position: "overImageOnBottom",
+        display: false
+
+    },
+
+    thumbnailHoverEffect2: "imageScaleIn80|imageSepiaOff|LabelApper75",
+    thumbanailGutterWidth: 20,
+    thumbanailGutterHeight: 20, 
+    thumbnailAlignment: "center",
+    thumbanailOpenImage: true
+
+  });
+
+
+
+
+
+});
